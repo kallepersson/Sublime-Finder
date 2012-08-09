727c519db819739aaa1f42c89dfdb307ce5ac1e7
kallepersson <hi@kallepersson.se>
2012-08-09 08:23:31 +0200

    Initial import

---
+++ finder.py
@@ -0,0 +1,27 @@
+import sublime
+import sublime_plugin
+import os
+import subprocess
+
+class FinderCommand():
+    def get_path(self, paths):
+        if paths:
+            return paths[0]
+        elif self.window.active_view():
+            return self.window.active_view().file_name()
+        elif self.window.folders():
+            return self.window.folders()[0]
+        else:
+            return '.'
+
+class OpenFinderCommand(sublime_plugin.WindowCommand, FinderCommand):
+    def run(self, paths=[], parameters=None):
+
+        path = self.get_path(paths)
+
+        if os.path.isfile(path):
+            path = os.path.dirname(path)
+
+        arg = ['open', path]
+        print arg
+        subprocess.Popen(arg)
